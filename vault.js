23b3477d2afd16e522d6a94417040357170c3b59
Kanojah <kanoacleveland@Kanojahs-MacBook-Pro.local>
2017-03-14 19:07:35 -1000

    pau with vault

--- vault.js
+++ vault.js
@@ -1,4 +1,20 @@
 'use strict';
 module.exports = function() {
-  
+
+  var vault = {};
+
+  function setValue(key, value){
+    vault[key] = value;
+    }
+
+  function getValue(key){
+    if(vault[key] === undefined){
+      return null;
+    }
+    return vault[key];
+  }
+  return {
+    setValue: setValue,
+    getValue: getValue
+  };
 };
